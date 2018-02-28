93be4bf7a0137008499d43c64d619cf5d2f364f9
Marius Hauken <marius.hauken@gmail.com>
2018-02-28 10:15:05 +0100

    Solution styled-components

--- src/styled-components/Feed.item.sc.js
+++ src/styled-components/Feed.item.sc.js
@@ -1,4 +1,8 @@
 import React from "react"
+import styled from "styled-components"
+import IconHeart from "../icons/Heart"
+import IconBubble from "../icons/Bubble"
+import IconMap from "../icons/Map"
 
 const FeedItemSC = ({
   fullname,
@@ -7,16 +11,127 @@ const FeedItemSC = ({
   timestamp,
   likes,
   avatar,
-  isNew
+  isNew,
+  color
 }) => (
-  <div>
-    {fullname}
-    {username}
-    {body}
-    {timestamp}
-    {likes}
-    <img src={avatar} alt={`Bilde av ${fullname}`} />
-  </div>
+  <FeedItem>
+    <Header>
+      <Avatar src={avatar} alt={`Bilde av ${fullname}`} />
+      <Author href="author">{fullname}</Author>
+      <Username>@{username}</Username>
+      <Time>{timestamp}</Time>
+    </Header>
+    <Body>{body}</Body>
+    <Footer color={color}>
+      <FeedBtn>
+        <IconBubble />
+      </FeedBtn>
+      <FeedBtn>
+        <IconHeart />
+        <BtnText>{likes}</BtnText>
+      </FeedBtn>
+      <FeedBtn>
+        <IconMap />
+        <BtnText>Location</BtnText>
+      </FeedBtn>
+    </Footer>
+  </FeedItem>
 )
+
+const FeedBtn = styled.button`
+  border: 0;
+  min-height: 36px;
+  vertical-align: middle;
+  text-align: left;
+  line-height: 36px;
+  background: transparent;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-right: ${p => p.theme.spacing.medium};
+  > svg {
+    width: 20px;
+  }
+  &:hover {
+    background-color: ${p => p.theme.color.line};
+  }
+`
+
+const BtnText = styled.span`
+  padding-left: ${p => p.theme.spacing.tiny};
+`
+
+const FeedItem = styled.div`
+  padding: ${p => p.theme.spacing.medium} ${p => p.theme.spacing.medium}
+    ${p => p.theme.spacing.small} ${p => p.theme.spacing.medium};
+  margin-bottom: ${p => p.theme.spacing.medium};
+  background-color: ${p => p.theme.color.bg};
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.03);
+`
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: ${p => p.theme.font.size.body};
+  > *:last-child {
+    text-align: right;
+    flex-grow: 2;
+  }
+`
+
+const Author = styled.a`
+  font-weight: bold;
+  color: ${p => p.theme.color.text};
+  text-decoration: none;
+  margin-left: ${p => p.theme.spacing.small};
+  &:hover,
+  &:active,
+  &:focus {
+    color: ${p => p.theme.color.primary};
+  }
+`
+
+const Username = styled.span`
+  color: ${p => p.theme.color.wcag};
+  &:before {
+    content: "·";
+    padding: 0 ${p => p.theme.spacing.tiny};
+  }
+`
+
+const Time = styled.time`
+  color: ${p => p.theme.color.secondary};
+`
+
+const Avatar = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  @media screen and (min-width: 740px) {
+    width: 54px;
+    height: 54px;
+  }
+`
+
+const Body = styled.p`
+  font-family: serif;
+  margin: ${p => p.theme.spacing.medium} 0;
+  font-size: ${p => p.theme.font.size.large};
+  line-height: 1.5;
+`
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding-top: ${p => p.theme.spacing.tiny};
+  border-top: 2px solid ${p => p.theme.color.line};
+  font-size: ${p => p.theme.font.size.legalese};
+  color: ${p => p.theme.color.wcag};
+  transition: border 0.2s ease;
+  ${FeedItem}:hover & {
+    border-top-color: ${p => p.color};
+  }
+`
 
 export default FeedItemSC
--- src/styled-components/Feed.sc.js
+++ src/styled-components/Feed.sc.js
@@ -1,7 +1,57 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import data from "../data/feed"
 import FeedItemSC from "./Feed.item.sc"
+
+const theme = {
+  color: {
+    text: "#2b2b2b",
+    primary: "#dc7429",
+    secondary: "#f27788",
+    bg: "#fbfbfa",
+    line: "#efede7",
+    mix: ["#dc7429", "#8ccfb9", "#88a0ba", "#f27788"],
+    wcag: "#787878"
+  },
+  spacing: {
+    large: "24px",
+    medium: "24px",
+    small: "12px",
+    tiny: "6px"
+  },
+  font: {
+    size: {
+      legalese: "12px",
+      body: "14px",
+      large: "20px"
+    }
+  }
+}
+
+const FeedSC = () => (
+  <ThemeProvider theme={theme}>
+    <Feed>
+      <Logo src="/img/logo.svg" alt="" />
+      <Wrapper>
+        <FeedList>
+          {data.map((item, index) => (
+            <FeedItemSC
+              fullname={item.fullname}
+              username={item.username}
+              body={item.body}
+              timestamp={item.timestamp}
+              likes={item.likes}
+              avatar={item.avatar}
+              isNew={item.new}
+              key={item.id}
+              color={theme.color.mix[index % theme.color.mix.length]}
+            />
+          ))}
+        </FeedList>
+      </Wrapper>
+    </Feed>
+  </ThemeProvider>
+)
 
 const Feed = styled.div`
   padding-top: 48px;
@@ -28,27 +78,5 @@ const Wrapper = styled.div`
 const FeedList = styled.ol`
   list-style-type: none;
 `
-
-const FeedSC = () => (
-  <Feed>
-    <Logo src="/img/logo.svg" alt="" />
-    <Wrapper>
-      <FeedList>
-        {data.map(item => (
-          <FeedItemSC
-            fullname={item.fullname}
-            username={item.username}
-            body={item.body}
-            timestamp={item.timestamp}
-            likes={item.likes}
-            avatar={item.avatar}
-            isNew={item.new}
-            key={item.id}
-          />
-        ))}
-      </FeedList>
-    </Wrapper>
-  </Feed>
-)
 
 export default FeedSC
